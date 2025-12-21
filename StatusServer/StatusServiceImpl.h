6218d2b1f9a16cbc58884f5ae17ccfd3175e4cc2
R-Poju <3681043633@qq.com>
2025-12-21 18:56:46 +0800

    StatusServer

--- StatusServer/StatusServiceImpl.h
+++ StatusServer/StatusServiceImpl.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <grpcpp/grpcpp.h>
 #include "message.grpc.pb.h"
+#include <mutex>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -8,11 +9,15 @@ using grpc::ServerContext;
 using grpc::Status;
 using message::GetChatServerReq;
 using message::GetChatServerRsp;
+using message::LoginReq;
+using message::LoginRsp;
 using message::StatusService;
 
 struct ChatServer {
 	std::string host;
 	std::string port;
+	std::string name;
+	int con_count;
 };
 
 class StatusServiceImpl final :public StatusService::Service
@@ -21,7 +26,14 @@ public:
 	StatusServiceImpl();
 	Status GetChatServer(ServerContext* context, const GetChatServerReq* request,
 		GetChatServerRsp* reply) override;
-	std::vector<ChatServer> _servers;
-	int _server_index;
+	Status Login(ServerContext* context, const LoginReq* request,
+		LoginRsp* reply) override;
+private:
+	void insertToken(int uid, std::string token);
+	ChatServer getChatServer();
+	std::unordered_map<std::string, ChatServer> _servers;
+	std::mutex _server_mtx;
+	std::unordered_map<int, std::string> _tokens;
+	std::mutex _token_mtx;
 };
 
