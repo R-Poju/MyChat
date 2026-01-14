5c0f96a9a857b29939d7aa3bc96f6346eb26dad1
R-Poju <3681043633@qq.com>
2026-01-14 21:34:03 +0800

    GateServer

--- GateServer/LogicSystem.cpp
+++ GateServer/LogicSystem.cpp
@@ -124,7 +124,7 @@ LogicSystem::LogicSystem() {
 
 		//先查找redis中email对应的验证码是否合理
 		std::string  varify_code;
-		bool b_get_varify = RedisMgr::GetInstance()->Get(CODEPREFIX+src_root["email"].asString(), varify_code);
+		bool b_get_varify = RedisMgr::GetInstance()->Get(CODEPREFIX + src_root["email"].asString(), varify_code);
 		if (!b_get_varify) {
 			std::cout << " get varify code expired" << std::endl;
 			root["error"] = ErrorCodes::VarifyExpired;
